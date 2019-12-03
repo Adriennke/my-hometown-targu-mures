8ba651d88b557c79eacee3f167cf92ceb76e833a
Adrienn <adrienntollas@gmail.com>
2019-12-03 13:17:39 +0100

    added more images

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
-let prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.bottom = "21";
-  } else {
-    document.getElementById("navbar").style.bottom = "-100px";
-  }
-  prevScrollpos = currentScrollPos;
-};
+// let prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//   let currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("ul").style.bottom = "1";
+//   } else {
+//     document.getElementById("ul").style.bottom = "11800px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// };
